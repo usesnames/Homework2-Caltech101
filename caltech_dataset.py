717f07be04342dfaa4410e818e4f59afd86b3ea7
usesnames <56475319+usesnames@users.noreply.github.com>
2020-05-01 23:04:42 +0200

    Add files via upload

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -19,28 +19,24 @@ class Caltech(VisionDataset):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
         self.split = split + ".txt"
-        with open(split) as f:
-            indexes = [line for line in f]
-        categories = [set(index[:-15] for index in indexes)].sort()
-
-        results = [__getitem__(index) for index in indexes]
-
-        return results
+        with open(self.split) as f:
+            self.indexes = [line.rstrip() for line in f if line.startswith("BAC")==False]
+        self.categories = list(set([ind[:-15] for ind in self.indexes]))
+        self.categories.sort(key=lambda v: v.upper())
+        self.labels = [self.categories.index(ind[:-15]) for ind in self.indexes]
+        self.images = [pil_loader("101_ObjectCategories/"+ind) for ind in self.indexes]
 
         #- Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class)
 
 
     def __getitem__(self, index):
-        int_index = int(index[-8:-4])
-        label = categories.index(index[:-15])
+            
+        #label = categories.index(index[:-15])
 
-        filepath = "101_ObjectCategories/" + index
-        image = pil_loader(filepath)
+        #image = pil_loader(index)
+        image, label = self.images[i], self.categories.index(self.indexes[i][:-15])
 
         # Applies preprocessing when accessing the image
-        if self.transform is not None:
-            image = self.transform(image)
-
         return image, label
 
     def __len__(self):
@@ -48,5 +44,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = 42 # Provide a way to get the length (number of elements) of the dataset
+        length = len(self.images) # Provide a way to get the length (number of elements) of the dataset
         return length
